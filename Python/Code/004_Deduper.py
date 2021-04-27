96bdf6c4bebdd08ca10a74f6222219c3f976569b
Arunpandi R <niveythaj5@gmail.com>
2021-04-27 08:20:50 +0530

    004_Deduper solution

--- Python/Code/004_Deduper.py
+++ Python/Code/004_Deduper.py
@@ -5,6 +5,15 @@ import random
 def deduper(raw_string):
     deduped_string = ''
     # Dedupe the string such that any number of occurrences of a letter is reduced to only one occurrence in the output.
+    string_list = list(raw_string)
+    conv_list = []
+    for idx, char in enumerate(string_list):
+        next_char = string_list[(idx + 1) % len(string_list)]
+        if char == next_char and char not in conv_list:
+            conv_list.append(char)
+        elif char not in conv_list:
+            conv_list.append(char)
+    deduped_string = ("".join(str(elem) for elem in sorted(conv_list)))
     return deduped_string
 
 # Driver
@@ -15,6 +24,7 @@ for i in range(25):
         num = random.randint(1, 5)
         raw_string += char * num
     output = deduper(raw_string)
+
 
 # Validation
     if output != base_string:
