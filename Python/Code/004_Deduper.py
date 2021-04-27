b109f25f48d94c717678e20f21c6e7057eae4438
Naveen <bnc1193@gmail.com>
2021-04-27 15:26:28 +0530

    004_deduper

--- Python/Code/004_Deduper.py
+++ Python/Code/004_Deduper.py
@@ -5,7 +5,6 @@ import random
 # Core Logic
 def deduper(raw_string):
     deduped_string = ''
-    # Dedupe the string such that any number of occurrences of a letter is reduced to only one occurrence in the output.
     for i in range(len(raw_string)):
         if raw_string[i] != raw_string[(i+1) % len(raw_string)] and \
                 raw_string[i] not in deduped_string:
@@ -13,7 +12,15 @@ def deduper(raw_string):
     return deduped_string
 
 
+def deduper_01(raw_string):
+    deduped_string = raw_string[0]
+    for character in raw_string:
+        if character not in deduped_string:
+            deduped_string += character
+    return deduped_string
+
 # Driver
+# base_string = 'abcdefghijklmnopqrstuvwxyz'
 base_string = 'abcdefabcdef'
 for i in range(25):
     raw_string = ''
